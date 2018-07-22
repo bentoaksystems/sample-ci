ab4d120fe0b9ccb334744beb79974dfedc9f47c0
arhariri <ali.71hariri@gmail.com>
2018-07-22 12:03:24 +0430

    inner-commits

---
+++ routes/api.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+const multer = require('multer');
+const path = require('path');
+
+
+function apiResponse() {
+  return (function (req, res) {
+    const isCommand = req.body.is_command;
+    const context = req.body.context;
+    const name = req.body.name;
+    const body = req.body.body;
+
+    // Check access (page and functionality (event))
+  });
+}
+
+router.use('/image', function (req, res, next) {
+
+});
+router.post('/image', apiResponse());
+
+router.post('/', apiResponse());
+
+module.exports = router;
