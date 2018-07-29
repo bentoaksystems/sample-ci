6b10b23b57b4673cbcaa294bb69a80c7df8a33aa
Mafphp <mafphp@gmail.com>
2018-07-29 18:01:51 +0430

    commited and pull from server

--- context/Sys/handler.js
+++ context/Sys/handler.js
@@ -8,7 +8,6 @@ const queries = {
   'getActions': require('./read-side/getActions'),
   'getRoles': require('./read-side/getRoles'),
   'getRoleAction': require('./read-side/getRoleAction'),
-  'deleteRoleAction': require('./read-side/deleteRoleAction'),
 }
 
 const commands = {
