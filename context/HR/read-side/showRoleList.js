35c90acac9f5ca6b4cfce3513b86952ffb52312a
imans777 <iman.s_sani@yahoo.com>
2018-08-05 18:17:44 +0430

    final changes for the server

--- context/HR/read-side/showRoleList.js
+++ context/HR/read-side/showRoleList.js
@@ -3,7 +3,7 @@ module.exports = async () => {
     try {
         const RoleRepo = require('../../Shared-Kernel/repositories/roleRepository');
         const roleRepo = new RoleRepo();
-        const roles = await roleRepo.load();
+        const roles = await roleRepo.getAll();
         return roles;
     } catch (err) {
         throw err;
