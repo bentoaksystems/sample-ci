40bd157259b4f3b8ae1d38cf03472268d140eb10
arhariri <ali.71hariri@gmail.com>
2018-08-07 11:47:21 +0430

    inner commits

--- context/reception/write-side/commands/admitPatient.js
+++ context/reception/write-side/commands/admitPatient.js
@@ -9,10 +9,19 @@ module.exports = class admitPatient extends BaseCommand {
 
   async execut(payload, user) {
     try {
-      ['firstname', 'surname', 'title', 'national_code', 'mobile_number', 'phone_number', 'patient_type_id', 'address'].forEach(el => {
-        if (!payload[el])
-          throw new Error('incomplete payload for adding a new patient');
-      });
+      const isSemiComplete = [
+        'firstname',
+        'surname',
+        'title',
+        'national_code',
+        'mobile_number',
+        'phone_number',
+        'patient_type_id',
+        'address'
+      ].every(el => !payload[el]);
+
+      if (!isSemiComplete || (!payload.birth_date && !payload.age))
+        throw new Error('incomplete payload for adding a new patient');
 
       const repo = new PatientRepository();
       const patient = await repo.findOrCreatePatient();
--- spec/reception/write-side/admitPatient.spec.js
+++ spec/reception/write-side/admitPatient.spec.js
@@ -19,7 +19,7 @@ describe('Admit patient in reception', () => {
     done();
   });
 
-  it('One with reception access should admit new dialysis patient', async function (done) {
+  it('One with reception access should admit new dialysis patient (both age and birth date are defined)', async function (done) {
     try {
       this.done = done;
 
@@ -40,6 +40,8 @@ describe('Admit patient in reception', () => {
             mobile_number: '9092301202',
             phone_number: '9092301202',
             patient_type_id: dialysisType.id,
+            birth_date: moment('1993-03-02'),
+            age: 25,
             address: {
               province: 'Tehran',
               city: 'Tehran',
@@ -59,6 +61,62 @@ describe('Admit patient in reception', () => {
       let emr = await EMR.model().findOne({where: {person_id: person.id}});
 
       expect(person).toBeDefined();
+      expect(moment(person.birth_date).format('YYYY-MM-DD')).toBe('1993-03-02');
+      expect(person.age).toBe(25);
+      expect(emr).toBeDefined();
+      expect(emr.exit_date).toBeNull();
+      expect(moment(emr.entry_date).format('YYYY-MM-DD')).toBe(moment().format('YYYY-MM-DD'));
+      expect(emr.patient_type_id).toBe(dialysisType.id);
+
+      done();
+    } catch (err) {
+      helper.errorHandler.bind(this)(err);
+    }
+  });
+
+  it('One with reception access should admit new dialysis patient (only age is defined)', async function (done) {
+    try {
+      this.done = done;
+
+      const dialysisType = await TypeDictionary.model().create({name: 'patient_type', type: 'dialysis'});
+
+      let res = await rp({
+        method: 'post',
+        uri: `${env.appAddress}/api`,
+        body: {
+          context: 'Reception',
+          is_command: true,
+          name: 'admitPatient',
+          payload: {
+            firstname: 'Ali',
+            surname: 'Alavi',
+            title: 'm',
+            national_code: '1234567890',
+            mobile_number: '9092301202',
+            phone_number: '9092301202',
+            patient_type_id: dialysisType.id,
+            age: 25,
+            address: {
+              province: 'Tehran',
+              city: 'Tehran',
+              district: 3,
+            }
+          }
+        },
+        jar: rpJar,
+        json: true,
+        resolveWithFullResponse: true,
+      });
+
+      expect(res.statusCode).toBe(200);
+      res = res.body;
+
+      let person = await Person.model().findOne({where: {id: res.id}});
+      let emr = await EMR.model().findOne({where: {person_id: person.id}});
+
+      expect(person).toBeDefined();
+      expect(person.birth_date).toBeNull();
+      expect(person.age).toBe(25);
       expect(emr).toBeDefined();
       expect(emr.exit_date).toBeNull();
       expect(moment(emr.entry_date).format('YYYY-MM-DD')).toBe(moment().format('YYYY-MM-DD'));
