8eacf4da8fee29ef3f29c92e9bbaf209a9eeecd1
Mafphp <mafphp@gmail.com>
2018-08-07 13:46:14 +0430

    commited :|

--- context/HR/handler.js
+++ context/HR/handler.js
@@ -1,21 +1,22 @@
 const BaseHandler = require('../../utils/base-handler');
 
 module.exports = class HRHandler extends BaseHandler {
-    constructor() {
-        const queries = {
-            'searchPersonnel': require('./read-side/searchPerson'),
-            'showOnePerson': require('./read-side/showOnePerson'),
-            'showRoleList': require('./read-side/showRoleList'),
-        };
+  constructor() {
+    const queries = {
+      searchPersonnel: require('./read-side/searchPerson'),
+      showOnePerson: require('./read-side/showOnePerson'),
+      showRoleList: require('./read-side/showRoleList'),
+      showingPatientInformation: require('./read-side/showingPatientInformation')
+    };
 
-        const commands = {
-            'definePersonnel': require('./write-side/commands/definePersonnel'),
-            'assignUserToPerson': require('./write-side/commands/assignUserToPerson'),
-            'updatePerson': require('./write-side/commands/updatePerson'),
-            'updatePersonRoles': require('./write-side/commands/updatePersonRoles'),
-            'deletePerson': require('./write-side/commands/deletePerson'),
-        };
+    const commands = {
+      definePersonnel: require('./write-side/commands/definePersonnel'),
+      assignUserToPerson: require('./write-side/commands/assignUserToPerson'),
+      updatePerson: require('./write-side/commands/updatePerson'),
+      updatePersonRoles: require('./write-side/commands/updatePersonRoles'),
+      deletePerson: require('./write-side/commands/deletePerson')
+    };
 
-        super(queries, commands);
-    }
-}
+    super(queries, commands);
+  }
+};
