92360d1dfbdec13aafcba5cd312918ebe19262eb
arhariri <ali.71hariri@gmail.com>
2018-08-08 17:15:18 +0430

    create models for dynamic policy

--- infrastructure/db/index.js
+++ infrastructure/db/index.js
@@ -18,6 +18,9 @@ const TypeDictionary = require('./models/type_dictionary.model');
 const Insurer = require('./models/insurer.model');
 const Form = require('./models/form.model');
 const FormField = require('./models/form_field.model');
+const ContextHook = require('./models/context_hook.model');
+const ContextHookPolicy = require('./models/context_hook_policy.model');
+const Checklist = require('./models/checklist.model');
 
 // namespace = cls.createNamespace('HIS-NS');
 // Sequelize.useCLS(namespace);
@@ -45,6 +48,9 @@ let tableList = [
   EMRDoc,
   TypeDictionary,
   Insurer,
+  ContextHook,
+  ContextHookPolicy,
+  Checklist,
 ];
 
 isReady = (isTest = false) => {
@@ -54,66 +60,88 @@ isReady = (isTest = false) => {
     logging: false
   });
 
-  return sequelize.authenticate()
+  let connect = () => sequelize.authenticate()
     .then(() => {
       console.log('-> ', 'Connection to db has been established successfully :)');
       tableList.forEach(model => {
         model.init(sequelize);
       });
 
-        /**
-         * Please define relations between tables (models) here
-         * (Not define in its model)
-         */
-        Page.model().hasMany(PageRole.model());
-        Role.model().hasMany(RoleAction.model());
-        Role.model().hasMany(PageRole.model());
-        Role.model().hasMany(Staff.model());
-        Action.model().hasMany(RoleAction.model());
-        RoleAction.model().belongsTo(Action.model());
-        RoleAction.model().belongsTo(Role.model());
-        Person.model().hasMany(Staff.model(), { onDelete: 'cascade' });
-        PageRole.model().belongsTo(Page.model());
-        PageRole.model().belongsTo(Role.model());
-        Staff.model().belongsTo(Person.model(), { onDelete: 'cascade' });
-        Staff.model().belongsTo(Role.model());
-        User.model().belongsTo(Person.model());
-        Person.model().hasOne(User.model(), {onDelete: 'cascade'});
-        Person.model().hasOne(EMR.model(), {onDelete: 'cascade'});
-        EMR.model().belongsTo(Person.model());
-        EMR.model().belongsTo(TypeDictionary.model(), { foreignKey: 'patient_type_id', sourceKey: 'id', as: 'patientType' });
-        EMR.model().belongsTo(TypeDictionary.model(), { foreignKey: 'regime_type_id', sourceKey: 'id' });
-        EMR.model().belongsTo(TypeDictionary.model(), { foreignKey: 'exit_type_id', sourceKey: 'id' });
-        TypeDictionary.model().hasMany(EMR.model(), { foreignKey: 'patient_type_id', sourceKey: 'id' });
-        TypeDictionary.model().hasMany(EMR.model(), { foreignKey: 'regime_type_id', sourceKey: 'id' });
-        TypeDictionary.model().hasMany(EMR.model(), { foreignKey: 'exit_type_id', sourceKey: 'id' });
-        EMR.model().belongsTo(Insurer.model());
-        Insurer.model().hasMany(EMR.model());
-        Document.model().belongsTo(User.model());
-        Document.model().belongsTo(TypeDictionary.model(), { foreignKey: 'document_type_id', sourceKey: 'id' });
-        TypeDictionary.model().hasMany(Document.model(), { foreignKey: 'document_type_id', sourceKey: 'id' });
-        EMRDoc.model().belongsTo(Document.model(), { onDelete: 'cascade' });
-        Document.model().hasMany(EMRDoc.model());
-        EMRDoc.model().belongsTo(TypeDictionary.model(), { foreignKey: 'emr_doc_type_id', sourceKey: 'id' });
-        TypeDictionary.model().hasMany(EMRDoc.model(), { foreignKey: 'emr_doc_type_id', sourceKey: 'id' });
-        EMRDoc.model().belongsTo(EMR.model(), { onDelete: 'cascade' });
-        EMR.model().hasMany(EMRDoc.model());
-        Address.model().belongsTo(Person.model(), { onDelete: 'cascade' });
-        Person.model().hasMany(Address.model());
-        User.model().hasMany(Form.model());
-        Form.model().belongsTo(User.model());
-        FormField.model().belongsTo(Form.model());
-        Form.model().hasMany(FormField.model());
+      /**
+       * Please define relations between tables (models) here
+       * (Not define in its model)
+       */
+      Page.model().hasMany(PageRole.model());
+      Role.model().hasMany(RoleAction.model());
+      Role.model().hasMany(PageRole.model());
+      Role.model().hasMany(Staff.model());
+      Action.model().hasMany(RoleAction.model());
+      RoleAction.model().belongsTo(Action.model());
+      RoleAction.model().belongsTo(Role.model());
+      Person.model().hasMany(Staff.model(), {onDelete: 'cascade'});
+      PageRole.model().belongsTo(Page.model());
+      PageRole.model().belongsTo(Role.model());
+      Staff.model().belongsTo(Person.model(), {onDelete: 'cascade'});
+      Staff.model().belongsTo(Role.model());
+      User.model().belongsTo(Person.model());
+      Person.model().hasOne(User.model(), {onDelete: 'cascade'});
+      Person.model().hasOne(EMR.model(), {onDelete: 'cascade'});
+      EMR.model().belongsTo(Person.model());
+      EMR.model().belongsTo(TypeDictionary.model(), {foreignKey: 'patient_type_id', sourceKey: 'id', as: 'patientType'});
+      EMR.model().belongsTo(TypeDictionary.model(), {foreignKey: 'regime_type_id', sourceKey: 'id'});
+      EMR.model().belongsTo(TypeDictionary.model(), {foreignKey: 'exit_type_id', sourceKey: 'id'});
+      TypeDictionary.model().hasMany(EMR.model(), {foreignKey: 'patient_type_id', sourceKey: 'id'});
+      TypeDictionary.model().hasMany(EMR.model(), {foreignKey: 'regime_type_id', sourceKey: 'id'});
+      TypeDictionary.model().hasMany(EMR.model(), {foreignKey: 'exit_type_id', sourceKey: 'id'});
+      EMR.model().belongsTo(Insurer.model());
+      Insurer.model().hasMany(EMR.model());
+      Document.model().belongsTo(User.model());
+      Document.model().belongsTo(TypeDictionary.model(), {foreignKey: 'document_type_id', sourceKey: 'id'});
+      TypeDictionary.model().hasMany(Document.model(), {foreignKey: 'document_type_id', sourceKey: 'id'});
+      EMRDoc.model().belongsTo(Document.model(), {onDelete: 'cascade'});
+      Document.model().hasMany(EMRDoc.model());
+      EMRDoc.model().belongsTo(TypeDictionary.model(), {foreignKey: 'emr_doc_type_id', sourceKey: 'id'});
+      TypeDictionary.model().hasMany(EMRDoc.model(), {foreignKey: 'emr_doc_type_id', sourceKey: 'id'});
+      EMRDoc.model().belongsTo(EMR.model(), {onDelete: 'cascade'});
+      EMR.model().hasMany(EMRDoc.model());
+      Address.model().belongsTo(Person.model(), {onDelete: 'cascade'});
+      Person.model().hasMany(Address.model());
+      User.model().hasMany(Form.model());
+      Form.model().belongsTo(User.model());
+      FormField.model().belongsTo(Form.model());
+      Form.model().hasMany(FormField.model());
+      ContextHook.model().belongsTo(Form.model());
+      Form.model().hasOne(ContextHook.model());
+      ContextHookPolicy.model().belongsTo(ContextHook.model());
+      ContextHook.model().hasMany(ContextHookPolicy.model());
+      ContextHookPolicy.model().belongsTo(Form.model(), {foreignKey: 'form_id', sourceKey: 'id'});
+      Form.model().hasMany(ContextHookPolicy.model(), {foreignKey: 'form_id', sourceKey: 'id'});
+      ContextHookPolicy.model().belongsTo(TypeDictionary.model(), {foreignKey: 'document_type_id', sourceKey: 'id'});
+      TypeDictionary.model().hasMany(ContextHookPolicy.model(), {foreignKey: 'document_type_id', sourceKey: 'id'});
+      ContextHookPolicy.model().belongsTo(Checklist.model(), {foreignKey: 'checklist_id', sourceKey: 'id'});
+      Checklist.model().hasMany(ContextHookPolicy.model(), {foreignKey: 'checklist_id', sourceKey: 'id'});
 
-        return isTest ? sequelize.sync({ force: true }) : sequelize.sync();
-        // return sequelize.sync({force: true});
-      })
-      .catch(err => {
-        console.error('-> ', 'Unable to connect to the database:', err);
-        setTimeout(connect, 1000);
-      });
+      return isTest ? sequelize.sync({force: true}) : sequelize.sync();
+      // return sequelize.sync({force: true});
+    })
+    .then(() => {
+      // Set constraints on tables manually
+      return Promise.all([
+        sequelize.query("ALTER TABLE context_hook_policy ADD CONSTRAINT unique_ids UNIQUE (form_id, checklist_id, document_type_id, context_hook_id);"),
+        sequelize.query("ALTER TABLE context_hook_policy ADD CONSTRAINT only_one_type CHECK (" +
+          "(form_id is not null and checklist_id is null and document_type_id is null) or" +
+          "(form_id is null and checklist_id is not null and document_type_id is null) or" +
+          "(form_id is null and checklist_id is null and document_type_id is not null)" +
+          ")"),
+      ]);
+    })
+    .catch(err => {
+      console.error('-> ', 'Unable to connect to the database:', err);
+      setTimeout(connect, 1000);
+    });
+
   return connect();
-  };
+};
 
 
 module.exports = {
