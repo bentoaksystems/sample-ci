297855659315b3881e0cf947c8ebb3de52d8e940
305-2 <sareh@bentoak.systems>
2018-08-08 10:58:35 +0430

    Merge branch 'master' of https://github.com/aminazar/his into new-form-builder
    # Conflicts:
    #	infrastructure/db/index.js

--- utils/pages.js
+++ utils/pages.js
@@ -3,5 +3,5 @@ module.exports  = [
   {name: 'تعریف پرسنل/کاربر' , url: 'hr/people'},
   {name: 'حقوق و دستمزد' , url: 'hr/payroll'},
   {name: 'طراحی فرم', url: 'forms/formBuilder'},
-  {name: ' ویراییش فرم', url: 'forms/editForm'},
+  {name: ' ویرایش فرم', url: 'forms/editForm'},
 ];
