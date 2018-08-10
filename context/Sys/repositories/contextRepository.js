312ce7ea68c4b07375f3b081623b25da592e58e8
Mafphp <mafphp@gmail.com>
2018-08-10 14:57:27 +0430

    create api and test

--- context/Sys/repositories/contextRepository.js
+++ context/Sys/repositories/contextRepository.js
@@ -7,8 +7,23 @@ class ContextRepository {
    * QUERY RELATED REPOSOTIROES:
    */
 
-  loadHooks() {
-    return ContextHook.model().findAll();
+  async loadHooks() {
+    const contexts = [];
+    const returnContextHooks = [];
+    await ContextHook.model()
+      .findAll({ raw: true })
+      .map(element => {
+        if (!contexts.includes(element.context)) {
+          contexts.push(element.context);
+          element.hooks = [element.hook];
+          delete element.hook;
+          returnContextHooks.push(element);
+        } else {
+          const findIndex = returnContextHooks.findIndex(el => el.context === element.context);
+          returnContextHooks[findIndex]['hooks'].push(element.hook);
+        }
+      });
+    return Promise.resolve(returnContextHooks);
   }
   /** COMMAND RELATED REPOSITORIES:
    * If a domain model is being requested by repositoris it should be returnd as an instance of domain model (new IForm())
