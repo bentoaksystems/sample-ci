45a3e77da0c4cf32aa4e75ff36548d29b5c8ce34
eabasir@gmail.com <eabasir@gmail.com>
2018-08-13 01:00:02 +0330

    c

---
+++ scripts/ready.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+RESULT="`wget -qO- http://localhost:$((80 + BUILD_NUMBER))/api/ready`"
+echo RESULT
+if [RESULT == 'server is fully up and running']
+then
+  return true
+else
+  return false
+fi
