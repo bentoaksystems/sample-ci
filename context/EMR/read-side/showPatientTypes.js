e4b90189d19bdd63fce009dff516110861fc4022
arhariri <ali.71hariri@gmail.com>
2018-08-12 19:22:03 +0430

    remove context directory

--- context/EMR/read-side/showPatientTypes.js
+++
@@ -1,9 +0,0 @@
-const TypeDictionaryRepository = require('../../Shared-Kernel/repositories/typeDicRepository');
-
-module.exports = async () => {
-  try {
-    return new TypeDictionaryRepository().showSystemTypes('patient', null, null, 1000);
-  } catch(err) {
-    throw err;
-  }
-}
--- context/reception/read-side/showExitTypes.js
+++
@@ -1,9 +0,0 @@
-const TypeDictionaryRepository = require('../../Shared-Kernel/repositories/typeDicRepository');
-
-module.exports = async () => {
-  try {
-    return new TypeDictionaryRepository().showSystemTypes('exit', null, null, 1000);
-  } catch (err) {
-    throw err;
-  }
-};
