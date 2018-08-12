5a68fb8a1e46d24a5c60ce570674635d824acd38
eabasir@gmail.com <eabasir@gmail.com>
2018-08-13 01:04:35 +0330

    c

--- scripts/ready.sh
+++ scripts/ready.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 RESULT="`wget -qO- http://localhost:$((80 + BUILD_NUMBER))/api/ready`"
-echo RESULT
-if [RESULT == 'server is fully up and running']
+echo $RESULT
+if [$RESULT == 'server is fully up and running']
 then
   return true
 else
