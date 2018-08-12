29949614d61059b1b748c6589e0a0cfc08d16d11
eabasir@gmail.com <eabasir@gmail.com>
2018-08-13 01:15:47 +0330

    c

--- scripts/ready.sh
+++ scripts/ready.sh
@@ -2,8 +2,7 @@
 RESULT="`wget -qO- http://localhost:$((80 + BUILD_NUMBER))/api/ready`"
 EXPECTED="server is fully up and running"
 echo $RESULT
-if [ "$RESULT" == "$EXPECTED" ]
-then
+if [ "$RESULT" == "$EXPECTED" ];then
   return true
 else
   return false
