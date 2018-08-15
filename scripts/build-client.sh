3fbd91d9ef6bb7d5ef3a79c5ef755e5bc6cae28d
eabasir@gmail.com <eabasir@gmail.com>
2018-08-15 12:12:13 +0330

    c

--- scripts/build-client.sh
+++ scripts/build-client.sh
@@ -8,13 +8,17 @@ if ls | grep ${GIT_BRANCH}-build
 then
   cp -R ${GIT_BRANCH}-client/* ${WORKSPACE}/public/
 else
+  echo 'current build dir not found on workspace... try to clone repository'
   git clone https://${GIT_CLIENT_CREDENTIALS}@${GIT_CLIENT_REPO}
   cd ./his-client
   git branch -r
-  TARGET="origin/master"
+  TARGET=""
   if git branch -r | grep "origin/${GIT_BRANCH}"
   then
     TARGET="origin/${GIT_BRANCH}" 
+  else
+    TARGET="origin/master"
+    echo 'same branch on client not found... try to build master branch'
   fi  
   git checkout -b origin/$TARGET -f 
   npm i
